4e46079cf1004017b0e930027501d90797447c75
Diaa-ibrahiem <diaaibrahiem646@gmail.com>
2020-12-13 19:18:30 +0200

    reset change password

--- src/app/main/pages/authentication/reset-password-2/reset-password-2.component.ts
+++ src/app/main/pages/authentication/reset-password-2/reset-password-2.component.ts
@@ -1,20 +1,30 @@
-import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
-import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
-import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { API_URLS } from "assets/constants/API_URLS";
+import { EntityService } from "./../../../../../@override/utils/entity.service";
+import { OverrideService } from "./../../../../../@override/utils/override.service";
+import { Router } from "@angular/router";
+import { Component, OnDestroy, OnInit, ViewEncapsulation } from "@angular/core";
+import {
+    AbstractControl,
+    FormBuilder,
+    FormGroup,
+    ValidationErrors,
+    ValidatorFn,
+    Validators,
+} from "@angular/forms";
+import { Subject } from "rxjs";
+import { takeUntil } from "rxjs/operators";
 
-import { FuseConfigService } from '@fuse/services/config.service';
-import { fuseAnimations } from '@fuse/animations';
+import { FuseConfigService } from "@fuse/services/config.service";
+import { fuseAnimations } from "@fuse/animations";
 
 @Component({
-    selector     : 'reset-password-2',
-    templateUrl  : './reset-password-2.component.html',
-    styleUrls    : ['./reset-password-2.component.scss'],
+    selector: "reset-password-2",
+    templateUrl: "./reset-password-2.component.html",
+    styleUrls: ["./reset-password-2.component.scss"],
     encapsulation: ViewEncapsulation.None,
-    animations   : fuseAnimations
+    animations: fuseAnimations,
 })
-export class ResetPassword2Component implements OnInit, OnDestroy
-{
+export class ResetPassword2Component implements OnInit, OnDestroy {
     resetPasswordForm: FormGroup;
 
     // Private
@@ -22,25 +32,27 @@ export class ResetPassword2Component implements OnInit, OnDestroy
 
     constructor(
         private _fuseConfigService: FuseConfigService,
-        private _formBuilder: FormBuilder
-    )
-    {
+        private _formBuilder: FormBuilder,
+        private enitiy: EntityService,
+        private override: OverrideService,
+        private router: Router
+    ) {
         // Configure the layout
         this._fuseConfigService.config = {
             layout: {
-                navbar   : {
-                    hidden: true
+                navbar: {
+                    hidden: true,
                 },
-                toolbar  : {
-                    hidden: true
+                toolbar: {
+                    hidden: true,
                 },
-                footer   : {
-                    hidden: true
+                footer: {
+                    hidden: true,
                 },
                 sidepanel: {
-                    hidden: true
-                }
-            }
+                    hidden: true,
+                },
+            },
         };
 
         // Set the private defaults
@@ -54,29 +66,47 @@ export class ResetPassword2Component implements OnInit, OnDestroy
     /**
      * On init
      */
-    ngOnInit(): void
-    {
+    ngOnInit(): void {
         this.resetPasswordForm = this._formBuilder.group({
-            name           : ['', Validators.required],
-            email          : ['', [Validators.required, Validators.email]],
-            password       : ['', Validators.required],
-            passwordConfirm: ['', [Validators.required, confirmPasswordValidator]]
+            email: ["", [Validators.required, Validators.email]],
+            oldPassword: ["", Validators.required],
+            password: ["", Validators.required],
+            passwordConfirm: [
+                "",
+                [Validators.required, confirmPasswordValidator],
+            ],
         });
 
         // Update the validity of the 'passwordConfirm' field
         // when the 'password' field changes
-        this.resetPasswordForm.get('password').valueChanges
-            .pipe(takeUntil(this._unsubscribeAll))
+        this.resetPasswordForm
+            .get("password")
+            .valueChanges.pipe(takeUntil(this._unsubscribeAll))
             .subscribe(() => {
-                this.resetPasswordForm.get('passwordConfirm').updateValueAndValidity();
+                this.resetPasswordForm
+                    .get("passwordConfirm")
+                    .updateValueAndValidity();
+            });
+    }
+
+    onSubmit() {
+        let data = this.resetPasswordForm.value;
+        this.enitiy
+            .save(API_URLS.auth.changePassword, data)
+            .subscribe((res: any) => {
+                if (res.status == 201) {
+                    this.enitiy.showSuccessToast(
+                        "successfully Change password"
+                    );
+                    this.router.navigate(["/apps", "dashboards", "project"]);
+                }
             });
     }
 
     /**
      * On destroy
      */
-    ngOnDestroy(): void
-    {
+    ngOnDestroy(): void {
         // Unsubscribe from all subscriptions
         this._unsubscribeAll.next();
         this._unsubscribeAll.complete();
@@ -89,30 +119,27 @@ export class ResetPassword2Component implements OnInit, OnDestroy
  * @param {AbstractControl} control
  * @returns {ValidationErrors | null}
  */
-export const confirmPasswordValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
-
-    if ( !control.parent || !control )
-    {
+export const confirmPasswordValidator: ValidatorFn = (
+    control: AbstractControl
+): ValidationErrors | null => {
+    if (!control.parent || !control) {
         return null;
     }
 
-    const password = control.parent.get('password');
-    const passwordConfirm = control.parent.get('passwordConfirm');
+    const password = control.parent.get("password");
+    const passwordConfirm = control.parent.get("passwordConfirm");
 
-    if ( !password || !passwordConfirm )
-    {
+    if (!password || !passwordConfirm) {
         return null;
     }
 
-    if ( passwordConfirm.value === '' )
-    {
+    if (passwordConfirm.value === "") {
         return null;
     }
 
-    if ( password.value === passwordConfirm.value )
-    {
+    if (password.value === passwordConfirm.value) {
         return null;
     }
 
-    return {passwordsNotMatching: true};
+    return { passwordsNotMatching: true };
 };
--- src/app/main/pages/authentication/reset-password/reset-password.component.ts
+++ src/app/main/pages/authentication/reset-password/reset-password.component.ts
@@ -1,46 +1,60 @@
-import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
-import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
-import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { API_URLS } from "./../../../../../assets/constants/API_URLS";
+import { ActivatedRoute, Router } from "@angular/router";
+import { OverrideService } from "./../../../../../@override/utils/override.service";
+import { EntityService } from "./../../../../../@override/utils/entity.service";
+import { Component, OnDestroy, OnInit, ViewEncapsulation } from "@angular/core";
+import {
+    AbstractControl,
+    FormBuilder,
+    FormGroup,
+    ValidationErrors,
+    ValidatorFn,
+    Validators,
+} from "@angular/forms";
+import { Subject } from "rxjs";
+import { takeUntil } from "rxjs/operators";
 
-import { FuseConfigService } from '@fuse/services/config.service';
-import { fuseAnimations } from '@fuse/animations';
+import { FuseConfigService } from "@fuse/services/config.service";
+import { fuseAnimations } from "@fuse/animations";
 
 @Component({
-    selector     : 'reset-password',
-    templateUrl  : './reset-password.component.html',
-    styleUrls    : ['./reset-password.component.scss'],
+    selector: "reset-password",
+    templateUrl: "./reset-password.component.html",
+    styleUrls: ["./reset-password.component.scss"],
     encapsulation: ViewEncapsulation.None,
-    animations   : fuseAnimations
+    animations: fuseAnimations,
 })
-export class ResetPasswordComponent implements OnInit, OnDestroy
-{
+export class ResetPasswordComponent implements OnInit, OnDestroy {
     resetPasswordForm: FormGroup;
-
+    email: string;
     // Private
     private _unsubscribeAll: Subject<any>;
 
     constructor(
         private _fuseConfigService: FuseConfigService,
-        private _formBuilder: FormBuilder
-    )
-    {
+        private _formBuilder: FormBuilder,
+        private enitiy: EntityService,
+        private override: OverrideService,
+        private router: Router,
+        private acroute: ActivatedRoute
+    ) {
+        this.email = this.acroute.snapshot.paramMap.get("email");
         // Configure the layout
         this._fuseConfigService.config = {
             layout: {
-                navbar   : {
-                    hidden: true
+                navbar: {
+                    hidden: true,
                 },
-                toolbar  : {
-                    hidden: true
+                toolbar: {
+                    hidden: true,
                 },
-                footer   : {
-                    hidden: true
+                footer: {
+                    hidden: true,
                 },
                 sidepanel: {
-                    hidden: true
-                }
-            }
+                    hidden: true,
+                },
+            },
         };
 
         // Set the private defaults
@@ -54,32 +68,58 @@ export class ResetPasswordComponent implements OnInit, OnDestroy
     /**
      * On init
      */
-    ngOnInit(): void
-    {
+    ngOnInit(): void {
         this.resetPasswordForm = this._formBuilder.group({
-            name           : ['', Validators.required],
-            email          : ['', [Validators.required, Validators.email]],
-            password       : ['', Validators.required],
-            passwordConfirm: ['', [Validators.required, confirmPasswordValidator]]
+            code: ["", Validators.required],
+            email: [this.email, [Validators.required, Validators.email]],
+            password: ["", Validators.required],
+            passwordConfirm: [
+                "",
+                [Validators.required, confirmPasswordValidator],
+            ],
         });
 
         // Update the validity of the 'passwordConfirm' field
         // when the 'password' field changes
-        this.resetPasswordForm.get('password').valueChanges
-            .pipe(takeUntil(this._unsubscribeAll))
+        this.resetPasswordForm
+            .get("password")
+            .valueChanges.pipe(takeUntil(this._unsubscribeAll))
             .subscribe(() => {
-                this.resetPasswordForm.get('passwordConfirm').updateValueAndValidity();
+                this.resetPasswordForm
+                    .get("passwordConfirm")
+                    .updateValueAndValidity();
             });
     }
 
     /**
      * On destroy
      */
-    ngOnDestroy(): void
-    {
+    ngOnDestroy(): void {
         // Unsubscribe from all subscriptions
         this._unsubscribeAll.next();
         this._unsubscribeAll.complete();
+    }
+
+    onSubmit() {
+        let data = this.resetPasswordForm.value;
+        this.enitiy
+            .save(
+                API_URLS.auth.resetPasswordbyCode.replace("{code}", data.code),
+                data,
+                this.override.AuthHeaders
+            )
+            .subscribe((res: any) => {
+                console.log("res", res);
+                if (res.status == 201) {
+                    this.router.navigate(["/pages", "auth", "login"]);
+                } else {
+                    this.router.navigate([
+                        "/pages/auth/reset-password",
+                        this.email,
+                    ]);
+                    this.enitiy.showErrorToast();
+                }
+            });
     }
 }
 
@@ -89,30 +129,27 @@ export class ResetPasswordComponent implements OnInit, OnDestroy
  * @param {AbstractControl} control
  * @returns {ValidationErrors | null}
  */
-export const confirmPasswordValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
-
-    if ( !control.parent || !control )
-    {
+export const confirmPasswordValidator: ValidatorFn = (
+    control: AbstractControl
+): ValidationErrors | null => {
+    if (!control.parent || !control) {
         return null;
     }
 
-    const password = control.parent.get('password');
-    const passwordConfirm = control.parent.get('passwordConfirm');
+    const password = control.parent.get("password");
+    const passwordConfirm = control.parent.get("passwordConfirm");
 
-    if ( !password || !passwordConfirm )
-    {
+    if (!password || !passwordConfirm) {
         return null;
     }
 
-    if ( passwordConfirm.value === '' )
-    {
+    if (passwordConfirm.value === "") {
         return null;
     }
 
-    if ( password.value === passwordConfirm.value )
-    {
+    if (password.value === passwordConfirm.value) {
         return null;
     }
 
-    return {passwordsNotMatching: true};
+    return { passwordsNotMatching: true };
 };
